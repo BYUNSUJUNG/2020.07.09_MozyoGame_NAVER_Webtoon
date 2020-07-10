5f12c20ac65105c5e2ea3d7713b61fa9baa1dfad
BYUNSUJUNG <susususu07@naver.com>
2020-07-11 02:12:13 +0900

    git init 2020-07-11

---
+++ MozyoGame2/Assets/Scripts/FindPan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FindPan : MonoBehaviour
+{
+	public GameObject[] obj = new GameObject[12];
+     
+
+    void Start()
+    {
+        
+        System.Random r = new System.Random();
+        for (int i = 0; i < 12; i++)
+        {
+            obj[i].transform.position = new Vector3 (r.Next(100, 900), r.Next(300, 800), 0);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}

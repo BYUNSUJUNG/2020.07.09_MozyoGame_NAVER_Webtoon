2e3337ec91b0c0aef2edfd3a9de11bbb84845520
BYUNSUJUNG <susususu07@naver.com>
2020-07-20 23:45:11 +0900

    git init 2020-07-20

---
+++ MozyoGame2/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+	public void ActivityScene() {
+		Debug.Log("활동이동");
+		SceneManager.LoadScene("ActivityScene");
+	}
+    public void StoreScene() {
+        Debug.Log("상점이동");
+		SceneManager.LoadScene("StoreScene");
+	}
+	public void BagScene() {
+        Debug.Log("가방이동");
+		SceneManager.LoadScene("BagScene");
+	}
+    public void IllustratedBookScene() {
+        Debug.Log("도감이동");
+		SceneManager.LoadScene("IllustratedBookScene");
+	}
+    
+    
+}

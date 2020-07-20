694d9ea0fc6201d68dc77741e2eeac70bc499b8a
BYUNSUJUNG <susususu07@naver.com>
2020-07-21 01:30:18 +0900

    git init 2020-07-21

---
+++ MozyoGame2/Assets/Scripts/ActivityImageChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivityImageChange : MonoBehaviour
+{
+    public GameObject[] obj = new GameObject[4];
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void MoneyActivity() {
+        obj[0].SetActive(true);
+        obj[1].SetActive(false);
+        obj[2].SetActive(false);
+        obj[3].SetActive(false);
+    }
+    public void StudyActivity() {
+        obj[0].SetActive(false);
+        obj[1].SetActive(true);
+        obj[2].SetActive(false);
+        obj[3].SetActive(false);
+    }
+}

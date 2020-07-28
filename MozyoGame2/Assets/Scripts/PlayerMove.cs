bcf78ee8ca0d158927bc36fed1ba44b3db76a342
BYUNSUJUNG <susususu07@naver.com>
2020-07-29 04:42:05 +0900

    git init 2020-07-29

---
+++ MozyoGame2/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();  
+    }
+    
+    void Update()
+    {
+        if (Input.GetButtonUp("Horizontal")) {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x *500f, rigid.velocity.y);
+        }
+        Debug.Log(Input.GetAxisRaw("Horizontal")); 
+    }
+/* 
+    void FixedUpdate()
+    {
+        // Move By Key Control
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        
+        if (rigid.velocity.x > maxSpeed) // Right Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * (-1))
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+    
+
+    }*/
+}

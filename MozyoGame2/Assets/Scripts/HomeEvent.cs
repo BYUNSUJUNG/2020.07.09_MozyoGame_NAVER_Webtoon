2af7b2832e8eb89347681e8e3d0d2ae6ae46f649
BYUNSUJUNG <susususu07@naver.com>
2020-08-03 21:28:33 +0900

    git init 2020-08-03

---
+++ MozyoGame2/Assets/Scripts/HomeEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeEvent : MonoBehaviour
+{
+    public GameObject Player;
+
+     void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log("충돌1");
+        }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("충돌");
+        if (collision.gameObject == Player) {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                SceneManager.LoadScene("HomeScene");
+            } 
+        }
+    }
+}
---
+++ MozyoGame2/Assets/Scripts/LibraryEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LibraryEvent : MonoBehaviour
+{
+    public GameObject Player;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("충돌");
+        if (collision.gameObject == Player) {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                SceneManager.LoadScene("LibraryScene");
+            } 
+        }
+    }
+}

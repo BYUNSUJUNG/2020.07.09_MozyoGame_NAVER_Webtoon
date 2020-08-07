f8b7cab027e2e817437b77070caaa215a125461c
BYUNSUJUNG <susususu07@naver.com>
2020-08-08 00:47:03 +0900

    git init 2020-08-08

---
+++ MozyoGame2/Assets/Scripts/SubwayEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SubwayEvent : MonoBehaviour
+{
+    public GameObject Player;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject == Player) {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                if(SceneManager.GetActiveScene().name == "SeaScene") {
+                    Debug.Log("타운 이동");
+		            SceneManager.LoadScene("TownScene");
+                } else if(SceneManager.GetActiveScene().name == "TownScene") {
+                    Debug.Log("바다 이동");
+		            SceneManager.LoadScene("SeaScene");
+                }
+               
+            } 
+        }
+    }
+}

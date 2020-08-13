e7912cc52d5b240be3730afb45ed036a1f816bba
BYUNSUJUNG <susususu07@naver.com>
2020-08-13 19:23:35 +0900

    git init 2020-08-13

---
+++ MozyoGame2/Assets/Scripts/Sea/TurkeyIcecreamEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TurkeyIcecreamEvent : MonoBehaviour
+{
+    public GameObject Player;
+
+    public GameObject TurkeyIcecreamObj;
+
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject == Player) {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                TurkeyIcecreamObj.SetActive(true);
+            } 
+        }
+    }
+}

f32b9ea13f2ca3d33e7e4d9fbf0475234b5942de
BYUNSUJUNG <susususu07@naver.com>
2020-08-13 18:05:22 +0900

    git init 2020-08-13

---
+++ MozyoGame2/Assets/Scripts/DollGameEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DollGameEvent : MonoBehaviour
+{
+    public GameObject Player;
+
+    public GameObject DollGameObj;
+
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject == Player) {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                DollGameObj.SetActive(true);
+            } 
+        }
+    }
+}
---
+++ MozyoGame2/Assets/Scripts/Mountain/PianoStudyEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PianoStudyEvent : MonoBehaviour
+{
+    public GameObject Player;
+
+    public GameObject PianoStudyObj;
+
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject == Player) {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                PianoStudyObj.SetActive(true);
+            } 
+        }
+    }
+}
